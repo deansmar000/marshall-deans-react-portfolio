473bd98ed7a6bb4b11342500ecfa5619aae98f58
MarshallDeans <marshalldns@gmail.com>
2019-07-09 21:37:48 -0700

    Updated portfolio, added navigation folder

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -9,6 +9,7 @@ export default class PortfolioContainer extends Component {
         
         this.state = {
             pageTitle: "Welcome to my portfolio",
+            isLoading: false,
             data:  [
                 { title: "Quip", category: "eCommerce" },
                 { title: "Eventbrite", category: "Scheduling"},
@@ -37,13 +38,22 @@ export default class PortfolioContainer extends Component {
     }
 
     render() {
+        if (this.state.isLoading) {
+            return <div>Loading...</div>
+        }
         return (
             <div>
                 <h2>{this.state.pageTitle}</h2>
 
-                <button onClick={() =>this.handleFilter("eCommerce")}>eCommerce</button>
-                <button onClick={() =>this.handleFilter("Scheduling")}>Scheduling</button>
-                <button onClick={() =>this.handleFilter("Enterprise")}>Enterprise</button>
+                <button onClick={() =>this.handleFilter("eCommerce")}>
+                    eCommerce
+                </button>
+                <button onClick={() =>this.handleFilter("Scheduling")}>
+                    Scheduling
+                </button>
+                <button onClick={() =>this.handleFilter("Enterprise")}>
+                    Enterprise
+                </button>
 
                 {this.PortfolioItems()}
 
