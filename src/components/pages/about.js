007f2b8afccf9bf33ca65111291f0b68622c9689
MarshallDeans <marshalldns@gmail.com>
2019-07-29 11:34:44 -0600

    Updated homepage styles

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -13,12 +13,14 @@ export default function() {
         }}
       />
       <div className="right-column">
-        I am Self motivated, Organized, and easy going. I enjoy building
-        websites that are functional and look aesthetically pleasing. My goal is
-        to accumulate the necessary knowledge required to provide for myself and
-        my family. Through previous work experience, I’ve gained a better
-        understanding of how to keep different things organized, which I’m
-        positive will be helpful while coding.
+        <p>
+          I am Self motivated, Organized, and easy going. I enjoy building
+          websites that are functional and look aesthetically pleasing. My goal
+          is to accumulate the necessary knowledge required to provide for
+          myself and my family. Through previous work experience, I’ve gained a
+          better understanding of how to keep different things organized, which
+          I’m positive will be helpful while coding.
+        </p>
       </div>
     </div>
   );
